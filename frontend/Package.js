08260f536e73b352d16530b610c5061cd7902d4a
balasanivenky8-source <balasanivenky8@gmail.com>
2026-09-12 16:05:36 +0530

    Create Package.js

---
+++ frontend/Package.js
@@ -0,0 +1,15 @@
+{
+  "name": "jarvis-ai",
+  "version": "1.0.0",
+  "type": "module",
+  "scripts": {
+    "start": "node server/server.js",
+    "dev": "node --watch server/server.js",
+    "test": "node --test"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.7",
+    "express": "^5.1.0"
+  }
+}
